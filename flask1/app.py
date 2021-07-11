3a5961672c532492cc55169f909d453956450aff
vijay241998 <51906586+vijay241998@users.noreply.github.com>
2021-07-11 20:06:26 +0530

    Update app.py

--- flask1/app.py
+++ flask1/app.py
@@ -71,7 +71,7 @@ def login():
 
         if is_valid:
             
-            json_userexist  = jsonify({"status": "userhelloo3 is valid"})
+            json_userexist  = jsonify({"status": "userworld is valid"})
             json_userexist.status_code = 200
         else:
             json_userexist  = jsonify({"error": "user is not valid"})
